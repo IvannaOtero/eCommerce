b946cec4be23b1b000bb2e348cb1216e4936f97b
IvannaOtero <iotero1982@students.cptc.edu>
2026-07-29 06:11:52 -0700

    Add ProductDBContext with SQL Server configuration
    Configured DI to add ProductDBContext using SQL Server.
    Connection string is read from "DefaultConnection" in config.

--- eCommerce/Program.cs
+++ eCommerce/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore; // <-- add this
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// DBContext Configuration
+builder.Services.AddDbContext<eCommerce.Data.ProductDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
