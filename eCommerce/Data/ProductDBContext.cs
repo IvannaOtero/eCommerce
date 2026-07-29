801f4b10ae1a1b705e7a9a4e69317399936ce103
IvannaOtero <iotero1982@students.cptc.edu>
2026-07-29 05:46:27 -0700

    Add EF Core support and ProductDBContext for Products
    Added EF Core dependencies to eCommerce.csproj for SQL Server and tooling. Introduced ProductDBContext class to manage Product entities using DbSet in the database.

---
+++ eCommerce/Data/ProductDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data;
+
+public class ProductDBContext : DbContext
+{
+    public ProductDBContext(DbContextOptions options) : base(options)
+    { 
+    
+    }
+
+    //Entities to be tracked by DbContext 
+    public DbSet<Models.Product> Products { get; set; }
+}
