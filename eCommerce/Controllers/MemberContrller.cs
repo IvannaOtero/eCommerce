aeb9b0cb7ffc0ab7b842a9f73ab1e52643a5a7fb
IvannaOtero <iotero1982@students.cptc.edu>
2026-08-19 05:56:09 -0700

    Add registration logic and improve validation scripts
    - Inject ProductDbContext into MemberController for DB access
    - Add [HttpPost] Register action to handle member registration
    - Update Register.cshtml to use @section Scripts for validation

--- eCommerce/Controllers/MemberContrller.cs
+++ eCommerce/Controllers/MemberContrller.cs
@@ -1,11 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.Controllers;
 
 public class MemberContrller : Controller
 {
+    private readonly ProductDbContext _context;
+
+    public MemberContrller(ProductDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Register()
     {
         return View(); 
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegistrationViewModel reg)
+    {
+        if (ModelState.IsValid)
+        {
+            Member newMember = new()
+            {
+                Username = reg.Username,
+                Email = reg.Email,
+                Password = reg.Password,
+                DateOfBirth = reg.DateOfBirth,
+            };
+
+            _context.Members.Add(newMember); 
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        return View(reg);
     }
 }
