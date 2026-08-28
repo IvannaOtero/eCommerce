f886ec8da56d2648795351f2e90ec116adbc9d09
IvannaOtero <iotero1982@students.cptc.edu>
2026-08-28 08:43:34 -0700

    Add LoginViewModel with required login properties
    Introduced LoginViewModel class containing required UsernameOrEmail and Password properties. Password is annotated with [DataType(DataType.Password)] for security.

--- eCommerce/Models/Member.cs
+++ eCommerce/Models/Member.cs
@@ -80,3 +80,11 @@ public class RegistrationViewModel
     
     public DateOnly DateOfBirth { get; set; }
 }
+
+public class LoginViewModel
+{
+    public required string UsernameOrEmail { get; set; }
+
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+}
